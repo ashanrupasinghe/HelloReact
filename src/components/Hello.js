fdd760b9d0edc3b56ca1582bc76dae26f9505bc8
ashan <ashanrupasinghe11@gmail.com>
2020-03-14 14:40:37 +0530

    without JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Hello = () => {
-	return (
+	/*return (
 	<div>
 		<h1>Hello Ashan</h1>
 	</div>
-	)
+	)*/
+	
+	//return React.createElement('div',null,'<h1>Helo Rupasinghe</h1>')
+	return React.createElement('div',null, React.createElement('h1',null,'Helo Rupasinghe'))
 }
  
  export default Hello;
